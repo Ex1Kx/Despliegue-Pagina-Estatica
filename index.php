87b306ddb2b6106304ddfc99d61dffec9a16e39f
Ex1Kx <thenewestekigoh@gmail.com>
2021-09-15 15:21:18 -0500

    Se Agrega Proyecto Para Su posterior Despliegue

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>INICIO</title>
+</head>
+<body>
+	<header>
+	<div class="contenido">
+		<div id="logo">
+			<h1>Diseño <span class="resal">Multimedia</span><h1>
+		</div>
+		<nav>
+			<ul>
+				<li class="bbtt"><a href="index.html"style="text-decoration:none">INICIO</li>
+				<li><a href="nosotros.html"style="text-decoration:none">NOSOTROS</li>
+				<li><a href="servicios.html"style="text-decoration:none">SERVICIOS</li>
+				<li><a href="contacto.html"style="text-decoration:none">CONTACTO</li>
+			</ul>
+		</nav>
+	</div>
+	</header>
+	<section id="headee">
+		<div class="contenido">
+			<h1>Expertos En Tecnologias Web</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+
+		</div>
+	</section>
+	<section id="bol">
+		<div class="contenido">
+			<h1>Suscribete A Nuestro Boletin</h1>
+			<form>
+				<input type="email" name="email" placeholder="Ingrese El Email.....">
+				<button type="submit" class="button1">Suscribete</button>
+			</form>
+		</div>
+	</section>
+	<section id="logos">
+		<div class="contenido">
+			<div class="logos">
+				<img src="images/logo_html.png">
+				<h3>HTML5</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+			</div>
+			<div class="logos">
+				<img src="images/logo_css.png">
+				<h3>CSS3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+		</div>
+		<div class="logos">
+				<img src="images/logo_brush.png">
+				<h3>DISEÑO GRAFICO</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+		</div>		
+	</section>
+	<footer>
+		<p>Graphel Desarrollo Web, Copyright © 2017</p>
+	</footer>
+</body>
+</html>
